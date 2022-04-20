0a5a7881657089a27f0347368ed6aa7be1e82409
Ali <deviran01@gmail.com>
2022-04-20 21:40:35 +0430

    Add round trip command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,10 @@
 Cypress.Commands.add('alibabaRootUrl', () => {
     cy.visit('https://www.alibaba.ir/')
 })
+
+Cypress.Commands.add('roundTrip', (destination) => {
+    cy.get('span').contains('یک طرفه').click()
+    cy.get('span').contains('رفت و برگشت').click()
+
+})
+
