ea557a044b39abe815a78732c24894ef60860636
Ali <deviran01@gmail.com>
2022-04-20 21:52:36 +0430

    Convert to commands

--- cypress/integration/Search/domestic_flight.spec.js
+++ cypress/integration/Search/domestic_flight.spec.js
@@ -4,11 +4,18 @@ describe('Search for Domestic flight tickets', () => {
       cy.alibaba_root_url()
     })
       it('Search tickets with start and end dates', () => {
-         
+        cy.get('[data-test=domestic-tab]').click()
+        cy.roundTrip()
+        cy.selectSource('تهران')
+        cy.destinationSource('مشهد')
  
       })
   
       it('Search tickets with time period ', () => {
+        cy.get('[data-test=domestic-tab]').click()
+        cy.roundTrip()
+        cy.selectSource('تهران')
+        cy.destinationSource('مشهد')
        
     })
     
