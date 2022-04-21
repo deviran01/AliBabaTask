28155f53ce17f03509d9da3b2c2bebd938bcb745
Ali <deviran01@gmail.com>
2022-04-21 14:33:19 +0430

    Update scenarios

--- cypress/integration/Search/domestic_flight.spec.js
+++ cypress/integration/Search/domestic_flight.spec.js
@@ -3,20 +3,20 @@ describe('Search for Domestic flight tickets', () => {
     beforeEach(() => {
       cy.alibabaRootUrl()
     })
-      it('Search tickets with start and end dates', () => {
-        cy.get('[data-test=domestic-tab]').click()
-        cy.roundTrip()
-        cy.selectSource('تهران')
-        cy.selectDestination('مشهد')
-        cy.dateSelector('2/2-2/7')
-        cy.numberOfPassengers('بزرگسال', 2)
-        cy.numberOfPassengers('کودک', 2)
-        cy.get('[name=search]').click()
-        cy.waitForSearchComplate()
-        cy.checkResultExist()
-        cy.contains('تماس با ما').scrollIntoView().should('be.visible').click()
+    //   it('Search tickets with start and end dates', () => {
+    //     cy.get('[data-test=domestic-tab]').click()
+    //     cy.roundTrip()
+    //     cy.selectSource('تهران')
+    //     cy.selectDestination('مشهد')
+    //     cy.dateSelector('2/2-2/7')
+    //     cy.numberOfPassengers('بزرگسال', 2)
+    //     cy.numberOfPassengers('کودک', 2)
+    //     cy.get('[name=search]').click()
+    //     cy.waitForSearchComplate()
+    //     cy.checkResultExist()
+    //     cy.contains('تماس با ما').scrollIntoView().should('be.visible').click()
  
-      })
+    //   })
   
       it('Search tickets with time period ', () => {
         cy.get('[data-test=domestic-tab]').click()
