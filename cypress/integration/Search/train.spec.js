2a93771ffd29e61f596810091a4ec7958c68e7d4
Ali <deviran01@gmail.com>
2022-04-22 20:18:33 +0430

    Add test for search train ticket

---
+++ cypress/integration/Search/train.spec.js
@@ -0,0 +1,51 @@
+/// <reference types="cypress" />
+describe('Search for international flight tickets', () => {
+    beforeEach(() => {
+        cy.aliBabaRootUrl()
+        cy.get('[data-test=train-tab]').click()
+
+    })
+    it('Search tickets with start and end dates', () => {
+        cy.roundTrip('رفت و برگشت')
+        cy.selectSource('مشهد')
+        cy.selectDestination('تهران')
+        cy.dateSelector('2/2-2/7')
+        cy.numberOfPassengers('بزرگسال', 2)
+        cy.numberOfPassengers('کودک', 2)
+        cy.get("button").contains('جستجو').click()
+        cy.waitForSearchComplate()
+        cy.checkResultExist()
+        cy.contains('تماس با ما').scrollIntoView().should('be.visible').click()
+
+    })
+
+    it('Search tickets with time period ', () => {
+        cy.roundTrip('رفت و برگشت')
+        cy.selectSource('مشهد')
+        cy.selectDestination('تهران')
+        cy.dateSelector(3)
+        cy.numberOfPassengers('بزرگسال', 2)
+        cy.numberOfPassengers('کودک', 2)
+        cy.get("button").contains('جستجو').click()
+        cy.waitForSearchComplate()
+        cy.checkResultExist()
+        cy.contains('تماس با ما').scrollIntoView().should('be.visible').click()
+
+    })
+
+    it('Search today tickets ', () => {
+        cy.roundTrip('یک طرفه')
+        cy.selectSource('مشهد')
+        cy.selectDestination('تهران')
+        cy.dateSelector('today')
+        cy.numberOfPassengers('بزرگسال', 2)
+        cy.numberOfPassengers('کودک', 2)
+        cy.get("button").contains('جستجو').click()
+        cy.waitForSearchComplate()
+        cy.checkResultExist()
+        cy.contains('تماس با ما').scrollIntoView().should('be.visible').click()
+
+    })
+
+
+})
